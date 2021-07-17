c44ce9fe36c895958bf62148d64f6f5141be7089
Cole <49996960+CWoulf@users.noreply.github.com>
2021-07-17 09:57:39 -0500

    caught up with accidental deletion

---
+++ Project Boost/Assets/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 10f;
+    [SerializeField] float rotationThrust = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+    void ProcessRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            Rotation(rotationThrust);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Rotation(-rotationThrust);
+        }
+    }
+
+    private void Rotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // unfreezing rotatino so the physics system can take over
+    }
+}
